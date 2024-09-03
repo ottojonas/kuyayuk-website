4228779c6c0331449edc4502363d71df57e92bd0
ottojonas <ottobjonas@outlook.com>
2024-09-04 00:03:14 +0100

    navbar.js LinkItem created

--- components/navbar.js
+++ components/navbar.js
@@ -1,3 +1,36 @@
-const LinkItem = ({}) => {}
+import NextLink from 'next/link'
+import {
+  Box,
+  Container,
+  Flex,
+  Heading,
+  Link,
+  Menu,
+  MenuButton,
+  MenuItem,
+  MenuList,
+  Stack,
+  IconButton,
+  useColorModeValue
+} from '@chakra-ui/react'
+import {} from '@chakra-ui/icons'
 
-const Navbar = () => {}
+const LinkItem = ({ href, path, children }) => {
+  const active = path === href
+  const inactiveColor = useColorModeValue('gray.800', 'whiteAlpha.900')
+  return (
+    <NextLink href={href} passHref>
+      <Link
+        p={2}
+        bg={active ? 'glassTeal' : undefined}
+        color={active ? '#202023' : inactiveColor}
+      >
+        {children}
+      </Link>
+    </NextLink>
+  )
+}
+
+const Navbar = props => {
+  const { path } = props
+}
