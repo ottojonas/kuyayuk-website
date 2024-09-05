a45846a9c39b51b9760a93819a62657fb368cc5e
ottojonas <ottobjonas@outlook.com>
2024-09-05 02:24:18 +0100

    navbar.js complete | layout/article.js layout/main.js completed

---
+++ components/layouts/main.js
@@ -0,0 +1,21 @@
+import { Box, Container } from '@chakra-ui/react'
+import Head from 'next/head'
+import Navbar from '../navbar'
+import Footer from '../footer'
+const Main = ({ children, router }) => {
+  return (
+    <Box as="main" pb={8}>
+      <Head>
+        <meta name="viewport" content="width = device-width, initial-scale=1" />
+        <title>KuyayUK</title>
+      </Head>
+      <Navbar path={router.asPath} />
+      <Container maxW="container.md" pt={14}>
+        {children}
+        <Footer />
+      </Container>
+    </Box>
+  )
+}
+
+export default Main
--- components/navbar.js
+++ components/navbar.js
@@ -13,8 +13,9 @@ import {
   IconButton,
   useColorModeValue
 } from '@chakra-ui/react'
-import {} from '@chakra-ui/icons'
-
+import Logo from './logo'
+import ThemeToggleButton from './toggle-theme-button'
+import { HamburgerIcon } from '@chakra-ui/icons'
 const LinkItem = ({ href, path, children }) => {
   const active = path === href
   const inactiveColor = useColorModeValue('gray.800', 'whiteAlpha.900')
@@ -33,4 +34,71 @@ const LinkItem = ({ href, path, children }) => {
 
 const Navbar = props => {
   const { path } = props
+  return (
+    <Box
+      position="fixed"
+      as="nav"
+      w="100%"
+      bg={useColorModeValue('#ffffff40', '#20202380')}
+      style={{ backdropFilter: 'blur(10px)' }}
+      zIndex={2}
+      {...props}
+    >
+      <Container
+        display="flex"
+        p={2}
+        maxW="container.md"
+        wrap="wrap"
+        align="center"
+        justify="space-between"
+      >
+        <Flex align="center" mr={5}>
+          <Heading as="h1" size="lg" letterSpacing={'tighter'}>
+            <Logo />
+          </Heading>
+        </Flex>
+        <Stack
+          direction={{ base: 'column', md: 'row' }}
+          display={{ base: 'none', md: 'flex' }}
+          width={{ base: 'full', md: 'auto' }}
+          alignItems="center"
+          flexGrow={1}
+          mt={{ base: 4, md: 0 }}
+        >
+          <LinkItem href="/products" path={path}>
+            Products
+          </LinkItem>
+          <LinkItem href="mailto:kuyayuk@outlook.com" path={path}>
+            Contact Us
+          </LinkItem>
+        </Stack>
+        <Box flex={1} align="right">
+          <ThemeToggleButton />
+          <Box ml={2} display={{ base: 'inline-block', md: 'none' }}>
+            <Menu>
+              <MenuButton
+                as={IconButton}
+                icon={<HamburgerIcon />}
+                variant="outline"
+                aria-label="Options"
+              />
+              <MenuList>
+                <NextLink href="/" passHref>
+                  <MenuItem as={Link}>Home</MenuItem>
+                </NextLink>
+                <NextLink href="/products" passHref>
+                  <MenuItem as={Link}>Products</MenuItem>
+                </NextLink>
+                <NextLink href="mailto:kuyayuk@outlook.com" passHref>
+                  <MenuItem as={Link}>Contact Us</MenuItem>
+                </NextLink>
+              </MenuList>
+            </Menu>
+          </Box>
+        </Box>
+      </Container>
+    </Box>
+  )
 }
+
+export default Navbar
