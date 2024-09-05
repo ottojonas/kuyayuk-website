3f1ae25213f100902fedcbeef4d1ffcd2f8b014e
ottojonas <ottobjonas@outlook.com>
2024-09-05 05:14:43 +0100

    added image of grandmother to front page | may be replaced - is simply just a placeholder for now

--- pages/index.js
+++ pages/index.js
@@ -15,6 +15,7 @@ import { GridItem } from '../components/grid-items'
 import Section from '../components/section'
 import { IoLogoInstagram } from 'react-icons/io5'
 import { EmailIcon } from '@chakra-ui/icons'
+import Image from 'next/image'
 import NextLink from 'next/link'
 import paloSantosThumbnail from '../public/images/paloSantos/paloSantosBuring01.jpg'
 import azufreThumbnail from '../public/images/azufre/azufreImage02.jpg'
@@ -36,6 +37,30 @@ const Page = () => {
         <Box>
           <Heading>KuyayUK</Heading>
           <p>Peruvian culture brought to you</p>
+        </Box>
+        <Box
+          flexShrink={0}
+          mt={{ base: 4, md: 0 }}
+          ml={{ md: 6 }}
+          textAlign="center"
+        >
+          <Box
+            borderColor="whiteAlpha.800"
+            borderWidth={2}
+            borderStyle="solid"
+            w="100px"
+            h="100px"
+            display="inline-block"
+            borderRadius="full"
+            overflow="hidden"
+          >
+            <Image
+              src="/icons/inspo/logoTemplate.png"
+              alt="frontImage"
+              width="100"
+              height="100"
+            />
+          </Box>
         </Box>
       </Box>
 
